45cf59e2ceb1575b42f3405033a92b4fccffb70e
SarayuYadaw <sarary264@gmail.com>
2020-07-22 16:43:42 +0530

    method is added

--- app/src/main/java/com/example/myapplication/add.java
+++ app/src/main/java/com/example/myapplication/add.java
@@ -2,4 +2,12 @@ package com.example.myapplication;
 
 public class add {
 
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int add(int a, int b,int c){
+        return a+b+c;
+    }
+
 }
